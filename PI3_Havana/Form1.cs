24bd6e0ec68dee769c986ba9687bb68696e184c1
Pedro Ferreira <pedroferreirafilipe@gmail.com>
2021-03-20 00:48:38 -0300

    sprint 3202021

--- PI3_Havana/Form1.cs
+++ PI3_Havana/Form1.cs
@@ -13,20 +13,23 @@ namespace PI3_Havana
 {
     public partial class Form1 : Form
     {
+        Session currentSession = new Session();
+
         public Form1()
         {
             InitializeComponent();
+
+            //Label .dll version
             lblVersion.Text = Jogo.Versao.ToString();
+            
+            //Lobby DataGrid
+            dgrLobby.DataSource = currentSession.currentList;
+            dgrLobby.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
-            string retorno = Jogo.ListarPartidas("T"); 
-            retorno = retorno.Replace("\r", "");
-            string[] linha = retorno.Split('\n');
-            for (int i = 0; i < linha.Length; i++) { 
-                listGames.Items.Add(linha[i]);
-            }
+            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -36,17 +39,7 @@ namespace PI3_Havana
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            listPlayers.Items.Clear();
-            string idPartida = listGames.SelectedItem.ToString();
-            string[] lista = idPartida.Split(',');
-            int id = Convert.ToInt32(lista[0]);
-            string retorno = Jogo.ListarJogadores(id);
-            //retorno = retorno.Replace("\r", "");
-            string[] linha = retorno.Split('\n');
-            for (int i = 0; i < linha.Length; i++)
-            {
-                listPlayers.Items.Add(linha[i]);
-            }
+
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -56,20 +49,318 @@ namespace PI3_Havana
 
         private void btnCreateGame_Click(object sender, EventArgs e)
         {
-            Jogo.CriarPartida(txtGameName.Text, txtPassword.Text);
+            try
+            {
+                //Gets text boxes content to create a new game
+                string getGameName = txtGameName.Text;
+                string getGamePassword = txtInputPassword.Text;
+                string response = Jogo.CriarPartida(getGameName, getGamePassword);
+
+                //Check if got an error message
+                string checkIfError = response.Substring(0, 4);
+
+                //Check if the message got a known error
+                if (checkIfError == "ERRO")
+                {
+                    //Shows returned errors
+                    MessageBox.Show(response);
+                }
+                else
+                {
+                    //Shows a message box when successful
+                    response = "New game created\nID: " + response;
+                    MessageBox.Show(response);
+                }
+            }
+            catch (Exception)
+            {
+                //Default message for unknown errors
+                MessageBox.Show("Couldn't perform this action, please try again.");
+            }
+            finally
+            {
+                txtGameName.Clear();
+                txtInputPassword.Clear();
+            }
         }
 
         private void btnEnterGame_Click(object sender, EventArgs e)
         {
-            string idPartida = listGames.SelectedItem.ToString();
-            string[] lista = idPartida.Split(',');
-            int id = Convert.ToInt32(lista[0]);
-            Jogo.EntrarPartida(id, txtPlayerName.Text, txtPasswordGame.Text);
-            Console.WriteLine("Branch testing");
+            try
+            {
+                //Gets a datagrid row and save its id for enter in a new game
+                var selectedRow = dgrLobby.SelectedRows[0].DataBoundItem as Game;
+                int selectedGameId = selectedRow.id;
+                
+                //Gets player name and password for the chosen game
+                string playerName = txtPlayerName.Text;
+                string gamePassword = txtPasswordGame.Text;
+                
+                //Enters in a new game and receives user information
+                string response = Jogo.EntrarPartida(selectedGameId, playerName, gamePassword);
+                
+                //Check if got an error message
+                string checkIfError = response.Substring(0, 4);
+
+                //Check if the message got a known error
+                if (checkIfError == "ERRO")
+                {
+                    //Shows returned errors
+                    MessageBox.Show(response);
+                }
+                else
+                {
+                    //Saves the information in an array and sets it to textboxes
+                    string[] playerInfo = response.Split(',');
+                    txtPlayerId.Text = playerInfo[0];
+                    txtPlayerPassword.Text = playerInfo[1];
+                    txtPlayerCollor.Text = playerInfo[2];
+                    txtPlayerName.Text = playerName;
+
+                    //Shows a message box when successful
+                    response = "You've entered in a new game match.";
+                    MessageBox.Show(response);
+                }
+
+            }
+            catch (Exception)
+            {
+                //Default message for unknown errors
+                MessageBox.Show("Couldn't perform this action, please try again.");
+            } 
+            finally
+            {
+                //Clear text boxes
+                txtPlayerName.Clear();
+                txtPasswordGame.Clear();
+            }
         }
 
         private void lvlVersion_Click(object sender, EventArgs e)
         {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listPlayers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listPlayers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listPlayers_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListGames_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgrLobby_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSelectGame_Click(object sender, EventArgs e)
+        {
+            /*try
+            {
+                //Gets a datagrid row and save its id for enter in a new game
+                var selectedRow = dgrLobby.SelectedRows[0].DataBoundItem as Game;
+                int selectedGameId = selectedRow.id;
+
+                //Gets list of players in selected game
+                string response = Jogo.ListarJogadores(selectedGameId);
+
+                //Check if got an error message
+                string checkIfError = response.Substring(0, 4);
+
+                //Check if the message got a known error
+                if (checkIfError == "ERRO")
+                {
+                    //Shows returned errors
+                    MessageBox.Show(response);
+                }
+                else
+                {
+                    //Construct the list of players and shows it
+                    string[] getListPlayers = response.Replace("\r", "").Split('\n');
+                    string[] listPlayers;
+                    ListBox lbListPlayers = new ListBox();
+                    foreach(string player in getListPlayers)
+                    {
+                        listPlayers = player.Split(',');
+                        lbListPlayers.Items.Add(listPlayers);
+                    }
+                    //lbListPlayers.Show();
+                    MessageBox.Show(Convert.ToString(listPlayers));
+                }
+            }
+            catch (Exception)
+            {
+
+                //Default message for unknown errors
+                MessageBox.Show("Couldn't perform this action, please try again.");
+            }*/
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Refreshes datagrid Lobby every 5 seconds
+            dgrLobby.DataSource = null;
+            dgrLobby.DataSource = currentSession.currentList;
+        }
+
+        private void txtPlayerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                //Get parameters
+                int playerId = Convert.ToInt16(txtPlayerId.Text);
+                string playerPassword = txtPlayerPassword.Text;
+
+                //Send initiante game command
+                string response = Jogo.IniciarPartida(playerId, playerPassword);
+                
+                //Check if got an error message
+                string checkIfError = response.Substring(0, 4);
+                
+                //Check if the message got a known error
+                if (checkIfError == "ERRO")
+                {
+                    //Shows returned errors
+                    MessageBox.Show(response);
+                }
+                else
+                {
+                    //Shows successful message
+                    MessageBox.Show("Game has started");
+                }
+            }
+            catch (Exception)
+            {
+                //Default message for unknown errors
+                MessageBox.Show("Couldn't perform this action, please try again.");
+            }
+        }
+
+        private void lbGameName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPasswordGame_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPlayerName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPasswordEnter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPlayerPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPlayerID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCollor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGameId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtGameName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sessionBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form1BindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- PI3_Havana/Game.cs
+++ PI3_Havana/Game.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using CantStopServer;
+
 
 namespace PI3_Havana
 {
     class Game
     {
-        public int idGame { get; set; }
-        public string name { get; set; }
-        public string data { get; set; }
-        public string senha { get; set;  }
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public string date { get; private set; }
+        public string status { get; private set;  }
+
+        private Game (int inputId, string inputName, string inputDate, string inputStatus)
+        {
+            id = inputId;
+            name = inputName;
+            date = inputDate;
+            status = inputStatus;
+        }
+        
+        public static List<Game> buildGameList()
+        {
+            //Response treatment
+            string response = Jogo.ListarPartidas("T");
+            List<Game> games = new List<Game>();
+            response = response.Replace("\r", "");
+            string[] line = response.Split('\n');
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                string[] items = line[i].Split(',');
+                Game createGame = new Game(Convert.ToInt16(items[0]), items[1], items[2], items[3]);
+                games.Add(createGame);
+            }
+            return games;
+        }
     }
 }
